7889b9372ffc1a76f0425413e08f02e8dd48e59f
Valentyn Bochkarov <bochkarov90@gmail.com>
2020-08-30 21:34:20 +0200

    Initial Commit

---
+++ ValAppStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ValAppStore
+//
+//  Created by Bochkarov Valentyn on 30/08/2020.
+//  Copyright © 2020 Bochkarov Valentyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
